95878313628594824e44510a5737da3fc0488606
Ruilx <799382072@qq.com>
2016-01-06 15:56:01 +0800

    update for 'empty' empty project Ruilx

--- About.h
+++ About.h
@@ -1,5 +1,35 @@
 #ifndef ABOUT
 #define ABOUT
 
+#include <QtCore>
+
+#define ApplicationName		"Source File Creator"
+#define OrganizationName	"GT-Soft Studio"
+#define AuthorName			"Ruilx"
+#define AuthorEmail			"zrrabbit@Gmail.com"
+#define ApplicationBugReportEmail "zrrabbit@Gmail.com"
+#define WebPage				"http://club.noahedu.com"
+#define UsingLicense		"General Public License"
+#define UsingLicenseVersion	0x00000300
+#define UsingSDK			"Qt"
+#define UsingSDKVersion		QT_VERSION
+#define ApplicationVersion	0x0D000100
+//                            | | | +-Revised Version (Patch)
+//                            | | +---Minor Version
+//                            | +-----Major Version
+//                            +-------Edition:
+//  0x00 = Stable
+//  0x01 = Standard
+//  0x0A = Alpha
+//  0x0B = Beta
+//  0x0C = Commercial (OpenSource)
+//  0x0D = Develop
+//  0x0E = Enhance
+//  0x0F = Final
+//  0x10 = Release
+//  0x11 = RC (Release Candidate)
+#define Version ApplicationVersion
+#define ApplicationLanguage 0x2052
+
 #endif // ABOUT
 
